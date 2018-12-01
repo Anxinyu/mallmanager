d904db58449178b9c488d97f39ae2b6350a9e44f
anxinyu <15010713090@163.com>
2018-12-01 11:23:03 +0800

    权限管理展开行完成

--- src/components/rights/roles.vue
+++ src/components/rights/roles.vue
@@ -4,10 +4,67 @@
       level1="权限管理"
       level2="角色列表"
     ></my-bread>
+    <el-button
+      type="primary"
+      plain
+      class="addRoleBtn"
+    >添加角色</el-button>
     <el-table
       :data="rolesList"
       style="width: 100%"
     >
+      <!-- 展开行 -->
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          <el-row
+            v-for="(item1,i) in scope.row.children"
+            :key="i"
+          >
+            <el-col :span="4">
+              <!-- 角色id和权限id -->
+              <el-tag
+                closable
+                @close="deleteRole(scope.row,item1.id)"
+              >{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row
+                v-for="(item2,i) in item1.children"
+                :key="i"
+              >
+                <el-col :span="4">
+                  <el-tag
+                    closable
+                    type="success"
+                    @close="deleteRole(scope.row,item2.id)"
+                  >{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    closable
+                    type="warning"
+                    @close="deleteRole(scope.row,item3.id)"
+                    v-for="(item3,i) in item2.children"
+                    :key="i"
+                  >{{item3.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col
+              v-if="scope.row.children.length===0"
+              :span="4"
+            >
+              未分配权限
+            </el-col>
+          </el-row>
+        </template>
+
+      </el-table-column>
       <el-table-column
         type="index"
         label="#"
@@ -63,21 +120,39 @@
 export default {
   data() {
     return {
-        rolesList:[]
+      rolesList: []
     };
   },
-  created(){
-      this.getRolesList()
+  created() {
+    this.getRolesList();
   },
   methods: {
-   async getRolesList() {
-     const res=await this.$http.get(`roles`);
-     const {data,meta:{msg,status}}=res.data;
-     this.rolesList=data;
+    async getRolesList() {
+      const res = await this.$http.get(`roles`);
+      console.log(res);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      this.rolesList = data;
+    },
+    // 展开行删除权限
+    deleteRole(role, rightId) {
+      this.$http.delete(`roles/${role.id}/rights/${rightId}`).then(res => {
+        console.log(res);
+        // 更新整个视图
+        // this.getRolesList();
+        // res中返回了200和剩余角色
+        const { data } = res.data;
+        role.children = data;
+      });
     }
   }
 };
 </script>
 
 <style>
+.addRoleBtn {
+  margin-top: 20px;
+}
 </style>
